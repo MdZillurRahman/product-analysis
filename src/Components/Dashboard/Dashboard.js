42e43246617450184be60eda7cca80d95c170dae
MdZillurRahman <mzisanr@gmail.com>
2022-04-05 01:51:24 +0600

    Dashboard Ready

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,81 @@
 import React from 'react';
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './Dashboard.css';
 
 const Dashboard = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
+        ;
     return (
-        <div>
-            <h2>Dashboard</h2>
+        <div className='dataChart'>
+            <div>
+                <h3>Investment VS Revenue</h3>
+                <BarChart width={550} height={250} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="investment" fill="#8884d8" />
+                    <Bar dataKey="revenue" fill="#82ca9d" />
+                </BarChart>
+            </div>
+            <div>
+                <h3>Investment VS Revenue</h3>
+                <PieChart width={730} height={250}>
+                    <Pie data={data} dataKey="investment" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+                    <Pie data={data} dataKey="revenue" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+                </PieChart>
+            </div>
+            <div className='lineChart'>
+                <h3>Investment VS Revenue</h3>
+                <LineChart width={550} height={250} data={data}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="investment" stroke="#8884d8" />
+                    <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
+                </LineChart>
+            </div>
         </div>
     );
 };
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import image from '../../images/Men-Genuine-Leather-Travel-Bag-in-Bangladesh.png';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <div>
-            <h1>Best Choice In Your Travelling</h1>
+        <div className='home'>
+            <div className='writtenPart'>
+                <h1>Best Choice In Your <span className='travelling'>Travelling</span></h1>
+                <p>We serve the best travelling bag in Bangladesh. Those travelling bag will be your best friend through your travelling journey.</p>
             </div>
-            <div></div>
+            <div>
+                <img src={image} alt="" />
+            </div>
         </div>
     );
 };

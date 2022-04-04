005b98bb19cda78cb8e7397442dc8f5cceae1edb
MdZillurRahman <mzisanr@gmail.com>
2022-04-05 00:58:07 +0600

    Navber Ready

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+            <h1>Best Choice In Your Travelling</h1>
+            </div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/Not Found/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import error from '../../images/lovepik-404-page-error-png-image.png';
+
+const NotFound = () => {
+    return (
+        <div>
+            <img src={error} alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
